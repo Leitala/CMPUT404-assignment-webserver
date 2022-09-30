a81ec26b949a4a823afdfb43966f58e1d7516202
Leitala <yhe7@ualberta.ca>
2022-09-29 23:31:18 -0600

    test pass
    pass the two tests

--- server.py
+++ server.py
@@ -1,14 +1,15 @@
-#  coding: utf-8 
+#  coding: utf-8
 import socketserver
+import os
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,11 +29,70 @@ import socketserver
 
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
+    def unsecure_formate(self):
+        directory = self.path.split("/")
+        if ".." in directory:
+            return True
+        else:
+            return False
+
+    def send_status_code(self, case):
+        match case:
+            case 405:
+                msg = self.protocol+ " 405 Method Not Allowed\r\n"
+                self.request.sendall(bytearray(msg,'utf-8'))
+                return
+            case 404:
+                msg = self.protocol + " 404 Not Found\r\n"
+                self.request.sendall(bytearray(msg,'utf-8'))
+                return
+            case 301:
+                msg = self.protocol + " 301 Moved Permanently\r\n"
+                self.request.sendall(bytearray(msg,'utf-8'))
+                return
+            case 200:
+                content_type = self.url_path.split(".")[-1]
+                mime_type = "Content-type: text/"+content_type
+                file = open(self.url_path, "r")
+                content = file.read()
+                msg = self.protocol +" 200 OK\r\n" + mime_type +"\n\n" + content
+                self.request.sendall(bytearray(msg,'utf-8'))
+                file.close()
+                return
+
+
+
     def handle(self):
         self.data = self.request.recv(1024).strip()
         print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+        # self.request.sendall(bytearray("OK",'utf-8'))
+        if self.data == b"":
+            self.send_status_code(404)
+        self.request_method, self.path, self.protocol = self.data.split(b"\r\n")[0].decode('utf-8').split(" ")
+
+        if "/www" not in self.path:
+            self.url_path = "./www" + self.path
+
+        if self.request_method == "GET":
+            if not os.path.exists(self.url_path) or self.unsecure_formate():
+                self.send_status_code(404)
+
+            elif os.path.isdir(self.url_path) and self.url_path[-1] != "/":
+                self.send_status_code(301)
+
+            else:
+                if os.path.isdir(self.url_path):
+                    self.url_path += "index.html"
+                self.send_status_code(200)
+        elif self.request_method == "POST" or self.request_method == "PUT" or self.request_method == "DELETE":
+            self.send_status_code(405)
+        else:
+            self.send_status_code(405)
+
+
+
+
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
